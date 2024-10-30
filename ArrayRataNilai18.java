1528162d0f29260d63c8f7b218f948ba9ac8b975
Nizam El <nizamel1402@gmail.com>
2024-10-30 09:28:52 +0700

    Pertemuan ke 9

---
+++ ArrayRataNilai18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print();
+        }
+
+
+
+    }
+}
