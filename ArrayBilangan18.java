a5f7cdf0d1b9ab2af67bf6b57c3945527bc2d5d1
Nizam El <nizamel1402@gmail.com>
2024-10-30 10:02:28 +0700

    pertemuan ke 9 No 4

--- ArrayBilangan18.java
+++ ArrayBilangan18.java
@@ -3,12 +3,10 @@
  */
 public class ArrayBilangan18 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-        for (int i=0; i<=4; i++) {
+        
+        double[] bil = { 5.0, 12867, 7.5, 2000000 };
+
+        for (int i=0; i<4; i++) {
             System.out.println(bil[i]);
         }
     }
