0c2eecdcd367e6ccdb5c4eb223ac795e1d97e2d7
Nizam El <nizamel1402@gmail.com>
2024-10-30 10:10:02 +0700

    percobaan ke 2 no 7

--- ArrayNilai18.java
+++ ArrayNilai18.java
@@ -3,12 +3,17 @@ public class ArrayNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
+
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Masukan nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " Tidak lulus!");
+            } 
         }
     }
 }
