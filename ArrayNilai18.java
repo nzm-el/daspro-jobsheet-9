d26d198d3d0f039dfc2dce85d0a334f92a65ce8f
Nizam El <nizamel1402@gmail.com>
2024-10-30 09:29:48 +0700

    pertemuan ke 9

--- ArrayNilai18.java
+++ ArrayNilai18.java
@@ -3,12 +3,12 @@ public class ArrayNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.print("Masukan nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+            System.out.println("Masukan nilai akhir ke-"+i+" adalah "+nilaiAkhir[i]);
         }
     }
 }
