cb7c8f185d43847c9f9b6fae27a8d98fa524db70
Nizam El <nizamel1402@gmail.com>
2024-10-31 12:24:55 +0700

    tugas 3

---
+++ Tugas318.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Tugas318{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean ketersediaan = false;
+
+        String menu[] = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", 
+                        "Kentang Goreng", "Teh Tarik", "Cappucino", 
+                        "Chocolate Ice"};
+
+        String masukkan;
+        System.out.print("Masukkan nama menu: ");
+        masukkan = sc.nextLine();
+
+        for (int i = 0; i < menu.length; i++) {
+            if (masukkan.equalsIgnoreCase(menu[i])) {
+                ketersediaan = true;
+                break;
+            } else {
+                ketersediaan = false;
+            }
+        }
+            
+        if(ketersediaan){
+            System.out.println("Menu Tersedia");
+        }else{
+            System.out.println("Menu Tidak Tersedia");
+        }
+    }   
+}
